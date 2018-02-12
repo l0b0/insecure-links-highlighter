039331278c0b9c422b7d9d5bfd956c9c207d06a8
Victor Engmark <victorengmark@catalyst.net.nz>
2018-02-12 16:00:49 +1300

    Split script into small functions

--- highlight.js
+++ highlight.js
@@ -1,21 +1,39 @@
 (function (exports) {
     "use strict";
 
+    if (typeof document !== "undefined") {
+        processDocument(document);
+    }
+
+    function processDocument(document) {
+        [].forEach.call(
+            getLinks(document),
+            highlightInsecureLinks
+        );
+    }
+
+    function getLinks(document) {
+        return document.getElementsByTagName("a");
+    }
+
+    function highlightInsecureLinks(element) {
+        if (!isSecureLink(element)) {
+            highlight(element);
+        }
+    }
+
+    function isSecureLink(element) {
+        return isSecure(element.getAttribute("href"));
+    }
+
     function isSecure(url) {
         return url.substring(0, 7) !== "http://";
     }
 
-    if (typeof document !== "undefined") {
-        [].forEach.call(
-            document.getElementsByTagName("a"),
-            function (element) {
-                var url = element.getAttribute("href");
-                if (!isSecure(url)) {
-                    element.style.border = "5px solid red";
-                }
-            }
-        );
+    function highlight(element) {
+        element.style.border = "5px solid red";
     }
 
     exports.isSecure = isSecure;
+    exports.highlight = highlight;
 }(this));
--- test/unit.js
+++ test/unit.js
@@ -6,9 +6,17 @@ var assert = require("assert"),
 describe("highlight", function() {
     "use strict";
 
-    describe("isSecure", function() {
+    describe(highlight.isSecure.name, function() {
         it("should return true for an HTTPS URL", function() {
             assert.ok(highlight.isSecure("https://example.org"));
+        });
+    });
+
+    describe(highlight.highlight.name, function() {
+        it("should set the style of the element", function() {
+            var element = {"style": {}};
+            highlight.highlight(element);
+            assert.equal(element.style.border, "5px solid red");
         });
     });
 });
