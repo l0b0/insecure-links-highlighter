234cab2182f8c901d574330538e16b62fe907510
Victor Engmark <victorengmark@catalyst.net.nz>
2018-02-14 16:27:57 +1300

    Tweak changelog markup
    - Remove redundant main heading
    - "Downgrade" <strong> to <em>
    - "Downgrade" <h2> to <strong>

--- ruby/changelog.sh
+++ ruby/changelog.sh
@@ -3,4 +3,4 @@
 set -o errexit -o nounset
 
 github_changelog_generator --no-verbose --output /tmp/CHANGELOG.md --project insecure-links-highlighter --user l0b0 >&2
-markdown /tmp/CHANGELOG.md | sed 's#</\?\(\(h[0-9]\)\|p\)>##g;'
+markdown /tmp/CHANGELOG.md | sed '/^<h1>/d;s#<\(/\?\)strong>#<\1em>#g;s#<\(/\?\)h2>#<\1strong>#g;s#</\?p>##g;'
