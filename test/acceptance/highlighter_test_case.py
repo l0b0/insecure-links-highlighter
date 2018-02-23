026e3a82704779cdb3143f286968ce68612bf74d
Victor Engmark <victorengmark@catalyst.net.nz>
2018-02-23 14:08:41 +1300

    Only capture screenshot when test fails
    Based on <https://stackoverflow.com/a/39606065/96588>.
    
    Resolves #19.

--- test/acceptance/highlighter_test_case.py
+++ test/acceptance/highlighter_test_case.py
@@ -19,7 +19,8 @@ class HighlighterTestCase(unittest.TestCase):
         )
 
     def tearDown(self):
-        self.driver.save_screenshot("/screenshots/{}.png".format(self.id()))
+        if not self.__successful_test():
+            self.driver.save_screenshot("/screenshots/{}.png".format(self.id()))
 
     @classmethod
     def tearDownClass(cls):
@@ -30,3 +31,16 @@ class HighlighterTestCase(unittest.TestCase):
 
     def assert_not_highlighted(self, element: WebElement):
         self.assertNotIn('insecure-links-highlighter-highlighted', get_classes(element))
+
+    def __successful_test(self) -> bool:
+        result = self.__load_test_result()
+
+        return not any(self.__problem_with_current_test(problems) for problems in [result.errors, result.failures])
+
+    def __problem_with_current_test(self, problems):
+        return problems and problems[-1][0] is self
+
+    def __load_test_result(self):
+        result = self.defaultTestResult()
+        self._feedErrorsToResult(result, self._outcome.errors)
+        return result
