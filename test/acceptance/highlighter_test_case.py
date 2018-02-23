684c33c1e39e7e9b8fe597165a25d82259138917
Victor Engmark <victorengmark@catalyst.net.nz>
2018-02-23 13:34:57 +1300

    Add type annotation

--- test/acceptance/highlighter_test_case.py
+++ test/acceptance/highlighter_test_case.py
@@ -1,6 +1,7 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
 
 from utilities import get_classes
 
@@ -24,8 +25,8 @@ class HighlighterTestCase(unittest.TestCase):
     def tearDownClass(cls):
         cls.driver.close()
 
-    def assert_highlighted(self, element):
+    def assert_highlighted(self, element: WebElement):
         self.assertIn('insecure-links-highlighter-highlighted', get_classes(element))
 
-    def assert_not_highlighted(self, element):
+    def assert_not_highlighted(self, element: WebElement):
         self.assertNotIn('insecure-links-highlighter-highlighted', get_classes(element))
--- test/acceptance/utilities.py
+++ test/acceptance/utilities.py
@@ -1,2 +1,7 @@
-def get_classes(element):
+from typing import List
+
+from selenium.webdriver.remote.webelement import WebElement
+
+
+def get_classes(element: WebElement) -> List[str]:
     return element.get_attribute('class').split()
