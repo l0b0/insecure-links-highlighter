be97d6e01a81b1aa55c1414ee03f59df8ed4b35d
Victor Engmark <victorengmark@catalyst.net.nz>
2018-02-26 17:15:40 +1300

    Use current directory in tests
    One step towards being able to run tests locally.

--- src/test/java/name/engmark/insecureLinksHighlighter/TestCase.java
+++ src/test/java/name/engmark/insecureLinksHighlighter/TestCase.java
@@ -15,6 +15,8 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 import static org.junit.Assert.assertFalse;
@@ -22,12 +24,14 @@ import static org.junit.Assert.assertTrue;
 
 public class TestCase {
     protected static WebDriver driver;
+    private static Path currentPath = Paths.get(System.getProperty("user.dir"));
 
     @Rule
     public TestWatcher watcher = new TestWatcher() {
         @Override
         protected void failed(Throwable testException, Description description) {
-            final String screenshotPath = String.format("/home/gradle/project/build/%s.png", description.getMethodName());
+            final String screenshotFilename = String.format("%s.png", description.getMethodName());
+            final String screenshotPath = Paths.get(currentPath.toString(), "build", screenshotFilename).toString();
             final File screenshotFile = new File(screenshotPath);
             final File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
             try {
@@ -45,7 +49,7 @@ public class TestCase {
 
         final FirefoxProfile profile = new FirefoxProfile();
         profile.setPreference("xpinstall.signatures.required", false);
-        final File extension = new File("/home/gradle/project/insecure-links-highlighter.xpi");
+        final File extension = new File(Paths.get(currentPath.toString(), "insecure-links-highlighter.xpi").toString());
         profile.addExtension(extension);
 
         final FirefoxOptions options = new FirefoxOptions();
